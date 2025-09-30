3201330ce01791d559beafcc90fbd45c513660d1
nolifeplayer <KAig19@nycstudents.net>
2025-09-30 12:37:46 -0400

    Animals and Food

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+        public GameObject projectilePrefab;
         public float horizontalInput;
         public float speed = 10.0f;
         public float xRange = 10;
@@ -22,7 +23,12 @@ public class PlayerController : MonoBehaviour
         }
         if (transform.position.x > xRange){
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        
+            }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
         
            
